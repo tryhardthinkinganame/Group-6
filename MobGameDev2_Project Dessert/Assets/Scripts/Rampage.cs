34c2825903eaae111764ee20e74c987accfdd9a0
tryhardthinkinganame <138574073+tryhardthinkinganame@users.noreply.github.com>
2023-09-27 19:15:54 +0800

    Final Proj

---
+++ MobGameDev2_Project Dessert/Assets/Scripts/Rampage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rampage : MonoBehaviour
+{
+    public void SugarRush()
+    {
+        SceneManager.LoadScene("Rampage");
+    }
+}
